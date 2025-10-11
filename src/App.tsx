10496a2b55091902392a11ac315838a3585c0846
Rino Godson <rinogodson@gmail.com>
2025-10-11 11:07:04 +0530

    fixed the over lightness on mobile screens

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
       <div className="jacquard-24-regular text-[#AEDEEC] text-4xl sm:text-6xl sm:h-25 flex justify-center items-center">
         Ghost Ink
       </div>
-      <div className="relative h-[calc(100%_-_10em)] sm:h-200 w-full mt-10 sm:mt-0 sm:w-150 flex justify-center items-center bg-white/5 border-3 border-[#AEDEEC] rounded-[3em] corner-scoop shadow-[inset_0_0_150px_rgba(174,222,236,0.4),_0_0_50px_#AEDEEC]">
+      <div className="relative h-[calc(100%_-_10em)] sm:h-200 w-full mt-10 sm:mt-0 sm:w-150 flex justify-center items-center bg-white/5 border-3 border-[#AEDEEC] rounded-[3em] corner-scoop shadow-[inset_0_0_150px_rgba(174,222,236,0.3),_0_0_50px_#AEDEEC] sm:shadow-[inset_0_0_150px_rgba(174,222,236,0.4),_0_0_50px_#AEDEEC]">
         <div className="h-fit w-fit sm:w-100">
           <TextBox />
         </div>
